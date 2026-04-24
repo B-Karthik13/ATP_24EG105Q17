44f50d3754c6f440eb1585693575bf0ddeb2a9f9
Karthik B <karthibandiga@gmail.com>
2026-04-24 15:33:01 +0530

    fix: CORS and Backend deployment config

--- BLOG_APPLICATION/Backend/server.js
+++ BLOG_APPLICATION/Backend/server.js
@@ -17,7 +17,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     // "https://blogapp-nine-rose-46.vercel.app"
-    "https://blogapplication-b-karthik13s-projects.vercel.app"
+    //"https://blogapplication-b-karthik13s-projects.vercel.app"
+    "https://blogapplication-snowy.vercel.app/"
   ],
   credentials: true
 }));
